94bedece341bb051280f3c0239edcc17ac95b378
lemaitre-aneo <flemaitre@aneo.fr>
2023-05-02 09:48:07 +0200

    ChunkAsync tests

--- csharp/src/Chunk.cs
+++ csharp/src/Chunk.cs
@@ -1,6 +1,6 @@
 ﻿// This file is part of the ArmoniK project
 //
-// Copyright (C) ANEO, 2022-2022.All rights reserved.
+// Copyright (C) ANEO, 2022-2023.All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ArmoniK.Utils;
 
@@ -71,6 +76,129 @@ internal static class Chunk
       {
         buffer[bufferSize] = e.Current;
       }
+    }
+  }
+
+
+  internal static async IAsyncEnumerable<T[]> IteratorAsync<T>(IAsyncEnumerable<T>                        enumerable,
+                                                               int                                        size,
+                                                               TimeSpan                                   maxDelay,
+                                                               [EnumeratorCancellation] CancellationToken cancellationToken = default)
+  {
+    var                      buffer      = Array.Empty<T>();
+    var                      bufferSize  = 0;
+    Exception?               error       = null;
+    Task?                    timeoutTask = null;
+    CancellationTokenSource? cts         = null;
+
+    void Clean()
+    {
+      cts?.Cancel();
+      timeoutTask?.Dispose();
+      cts?.Dispose();
+      timeoutTask = null;
+      cts         = null;
+    }
+
+    cancellationToken.ThrowIfCancellationRequested();
+    await using var enumerator = enumerable.GetAsyncEnumerator(cancellationToken);
+    var nextTask = enumerator.MoveNextAsync()
+                             .AsTask();
+
+    while (true)
+    {
+      using var which = timeoutTask is null
+                          ? nextTask
+                          : await Task.WhenAny(nextTask,
+                                               timeoutTask)
+                                      .ConfigureAwait(false);
+
+      try
+      {
+        await which.ConfigureAwait(false);
+      }
+      catch (Exception e)
+      {
+        error = e;
+        break;
+      }
+
+      if (ReferenceEquals(which,
+                          timeoutTask))
+      {
+        Debug.Assert(bufferSize > 0);
+        Array.Resize(ref buffer,
+                     bufferSize);
+        timeoutTask.Dispose();
+        timeoutTask = null;
+        yield return buffer;
+
+        buffer     = new T[bufferSize];
+        bufferSize = 0;
+        continue;
+      }
+
+      Debug.Assert(ReferenceEquals(which,
+                                   nextTask));
+      if (!await nextTask.ConfigureAwait(false))
+      {
+        break;
+      }
+
+      if (bufferSize == buffer.Length)
+      {
+        Debug.Assert(buffer.Length < size);
+        var newLength = Math.Min(Math.Max(buffer.Length + buffer.Length / 2,
+                                          4),
+                                 size);
+
+        Array.Resize(ref buffer,
+                     newLength);
+      }
+
+      buffer[bufferSize] =  enumerator.Current;
+      bufferSize         += 1;
+
+      if (bufferSize == buffer.Length && bufferSize >= size)
+      {
+        Clean();
+        yield return buffer;
+        buffer     = new T[bufferSize];
+        bufferSize = 0;
+      }
+      else if (timeoutTask is null)
+      {
+        cts ??= CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        timeoutTask = Task.Delay(maxDelay,
+                                 cts.Token);
+      }
+
+      try
+      {
+        cancellationToken.ThrowIfCancellationRequested();
+        nextTask = enumerator.MoveNextAsync()
+                             .AsTask();
+      }
+      catch (Exception e)
+      {
+        error = e;
+        break;
+      }
+    }
+
+    if (bufferSize > 0)
+    {
+      Array.Resize(ref buffer,
+                   bufferSize);
+      yield return buffer;
+    }
+
+    Clean();
+
+    if (error is not null)
+    {
+      ExceptionDispatchInfo.Capture(error)
+                           .Throw();
     }
   }
 }
